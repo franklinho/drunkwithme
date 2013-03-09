a982bc8a8da32c4b7636b5dee800d05e68412e69
Rob McQueen <rob@mopub.com>
2013-03-09 14:02:51 -0800

    changing around organization of code for django

---
+++ drunkwithme/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+urlpatterns = patterns('',
+    url(r'', include('mobile.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT}),
+)
+
+urlpatterns += staticfiles_urlpatterns()
