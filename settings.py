be633dd9bae2efbb780a5d61fd97f5ea1e68f52e
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-10 01:13:22 +0000

    fixing settings file htiing

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "main.context_processors_domain_url"
+    "main.context_processors.domain_url"
     )
 
 TEMPLATE_DIRS = (
