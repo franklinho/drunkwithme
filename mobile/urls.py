a0c6948648c2711dd8c32146ed96f671758e8381
Franklin Ho <franklineho@gmail.com>
2013-03-10 01:52:46 +0100

    added links

--- mobile/urls.py
+++ mobile/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('mobile.views',
     url(r'^$', 'index',name="mobile_index"),
+    url(r'^map/$'), 'map',name="map"),
+	url(r'^drink/$'), 'drink',name="drink"),
+	url(r'^rank/$'), 'rank',name="rank"),
+	url(r'^stats/$'), 'stats',name="stats"),
+
 )
--- mobile/views.py
+++ mobile/views.py
@@ -3,3 +3,9 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response("HelloMap.html",{},RequestContext(request))
+
+def map(request):
+    return render_to_response("HelloMap.html",{},RequestContext(request))
+
+def drink(request):
+    return render_to_response("Drink.html",{},RequestContext(request))
