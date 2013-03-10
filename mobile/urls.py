55d34a043c135b69f79af9e3dc46c024383812f2
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-10 02:05:12 +0000

    IT WORKS

--- mobile/urls.py
+++ mobile/urls.py
@@ -4,7 +4,8 @@ urlpatterns = patterns('mobile.views',
     url(r'^$', 'index',name="mobile_index"),
     url(r'^drink/$', 'drink',name="mobile_drink"),
     url(r'^map/$', 'map',name="map"),
-    url(r'^drink/(?P<drink_id>.*)/$', 'drink_action',name="drink_action"),
+    url(r'^drink-action/(?P<drink_id>.*)/$', 'drink_action',name="drink_action"),
+    url(r'^whoami/$', 'whoami',name="mobile_whoami"),
     # url(r'^rank/$', 'rank',name="rank"),
     # url(r'^stats/$', 'stats',name="stats")
 )
--- mobile/views.py
+++ mobile/views.py
@@ -1,23 +1,22 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.template import RequestContext
+from django.conf import settings
+from main.models import Drink
 import facebook
 
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render_to_response("HelloMap.html",{},RequestContext(request))
 
+@login_required
 def drink_action(request,drink_id):
     graph = facebook.GraphAPI(request.user.social_auth.all()[0].extra_data['access_token'])
     drink = get_object_or_404(Drink,pk=drink_id)
-    try:
-        graph.put_object("me","drunkwithmeapp:take",drink=drink.facebook_object_url)
-        return HttpResponse("OK")
-    except Exception:
-        return HttpResponseBadRequest("Failed")
-    
-    return HttpResponse("Not sure what happeneded....")
+    graph.put_object("me","drunkwithmeapp:take",drink="%s%s" % (settings.DOMAIN_URL,drink.facebook_object_url))
+    return HttpResponse("OK")
     
 def map(request):
     return render_to_response("HelloMap.html",{},RequestContext(request))
@@ -25,3 +24,6 @@ def map(request):
 def drink(request):
     return render_to_response("Drink.html",{},RequestContext(request))
 
+
+def whoami(request):
+    return HttpResponse(request.user)
