0f9800ae63a94232a7a0f67c4f3d790528e2da0e
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-10 01:35:43 +0000

    chaning url to drink instead of check_in

--- mobile/urls.py
+++ mobile/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('mobile.views',
     url(r'^$', 'index',name="mobile_index"),
-    url(r'^check_in/$', 'check_in',name="mobile_check_in"),
+    url(r'^drink/$', 'drink',name="mobile_drink"),
     url(r'^map/$', 'map',name="map"),
     url(r'^drink/$', 'drink',name="drink"),
     # url(r'^rank/$', 'rank',name="rank"),
