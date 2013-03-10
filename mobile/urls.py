341b227d8af56ca3ec97412aba3eb61edc521fd0
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-10 03:45:17 +0000

    derp

--- mobile/urls.py
+++ mobile/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('mobile.views',
     url(r'^map/$', 'map',name="map"),
     url(r'^drink-action/(?P<drink_id>.*)/$', 'drink_action',name="drink_action"),
     url(r'^checkin-action/(?P<bar_id>.*)/$', 'checkin_action',name="checkin_action"),
-    url(r'^whoami/$', 'whoami',name="mobile_whoami"),
     url(r'^rank/$', 'rank',name="rank"),
     url(r'^stats/$', 'stats',name="stats")
 )
