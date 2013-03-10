5d1ff2e43932a1e597f71e929790ac3298a26c38
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-10 01:05:44 +0000

    not login_required for now

--- mobile/views.py
+++ mobile/views.py
@@ -5,11 +5,9 @@ import facebook
 
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def index(request):
     return render_to_response("HelloMap.html",{},RequestContext(request))
 
-@login_required
 def check_in(request):
     graph = facebook.GraphAPI(request.user.social_auth.all()[0].extra_data['access_token'])
 
