a6428d0d0130897a4c5fdd4a37a714e86160ce0e
Ubuntu <ubuntu@ip-10-118-109-156.ec2.internal>
2013-03-16 04:46:26 +0000

    adding gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,7 @@
+# see http://gunicorn.org/configure.html                                                                                                                                         
+import os
+__pwd = os.path.abspath(__file__)
+workers = 5
+timeout = 120
+bind = "127.0.0.1:5008"
+loglevel = 'debug'
